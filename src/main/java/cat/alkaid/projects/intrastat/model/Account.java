d34363cd6a7f482a66f77ce6137af92ec197b90d
Xavier Cortada <xavier.cortada@yahoo.es>
2020-11-14 11:12:19 +0100

    Decompiled

--- src/main/java/cat/alkaid/projects/intrastat/model/Account.java
+++ src/main/java/cat/alkaid/projects/intrastat/model/Account.java
@@ -1,137 +1,230 @@
 package cat.alkaid.projects.intrastat.model;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.persistence.PostLoad;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.HashSet;
+import javax.persistence.TemporalType;
+import javax.persistence.Temporal;
+import java.util.Date;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import javax.persistence.Embedded;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.persistence.Entity;
 import java.io.Serializable;
-import java.util.Date;
 
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.Version;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@SuppressWarnings("serial")
 @Entity
 @XmlRootElement
-public class Account implements Serializable {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
-	private Long id;
-	@Version
-	@Column(name = "version")
-	private int version;
-
-	@OneToOne(mappedBy = "account")
-	private
-	User user;
-	
-	private String userName;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Account implements Serializable
+{
+    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final String SECRET = "0123456789abcdef";
+    private static final String INITIALIZATIO_VECTOR = "AODVNUASDNVVAOVF";
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    @Version
+    @Column(name = "version")
+    private int version;
+    private String userName;
     private String password;
     private String token;
-
+    private String activationCode;
+    @Transient
+    private Long activeCompany;
+    @Transient
+    private String plainPwd;
+    @Embedded
+    private Address address;
+    @Embedded
+    @JsonUnwrapped
+    private Person person;
+    @OneToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER, mappedBy = "account")
+    private Set<Company> companies;
     @Temporal(TemporalType.DATE)
     private Date activated;
-
-
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(final Long id) {
-		this.id = id;
-	}
-
-	public int getVersion() {
-		return this.version;
-	}
-
-	public void setVersion(final int version) {
-		this.version = version;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Account)) {
-			return false;
-		}
-		Account other = (Account) obj;
-		if (id != null) {
-			if (!id.equals(other.id)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	
-    public void changePassword(String password) {
-        this.setPassword(password);
+    
+    public Account() {
+        this.companies = new HashSet<Company>();
     }
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-	public Date getActivated() {
-		return activated;
-	}
-
-	public void setActivated(Date activated) {
-		this.activated = activated;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	@Override
-	public String toString() {
-		return "Account [id=" + id + ", user=" + user + "]";
-	}
-
-	public boolean validate(String password2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(final Long id) {
+        this.id = id;
+    }
+    
+    public int getVersion() {
+        return this.version;
+    }
+    
+    public void setVersion(final int version) {
+        this.version = version;
+    }
+    
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Account)) {
+            return false;
+        }
+        final Account other = (Account)obj;
+        return this.id == null || this.id.equals(other.id);
+    }
+    
+    public String getActivationCode() {
+        return this.activationCode;
+    }
+    
+    public void setActivationCode(final String activationCode) {
+        this.activationCode = activationCode;
+    }
+    
+    public void changePassword(final String password) {
+        this.setPlainPwd(password);
+    }
+    
+    @JsonIgnore
+    public String getPassword() {
+        return this.password;
+    }
+    
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+    
+    public String getToken() {
+        return this.token;
+    }
+    
+    public void setToken(final String token) {
+        this.token = token;
+    }
+    
+    public Date getActivated() {
+        return this.activated;
+    }
+    
+    public void setActivated(final Date activated) {
+        this.activated = activated;
+    }
+    
+    public String getUserName() {
+        return this.userName;
+    }
+    
+    public void setUserName(final String userName) {
+        this.userName = userName;
+    }
+    
+    public boolean validate(final String password) {
+        return this.plainPwd.equals(password);
+    }
+    
+    public Address getAddress() {
+        return this.address;
+    }
+    
+    public void setAddress(final Address address) {
+        this.address = address;
+    }
+    
+    public Person getPerson() {
+        return this.person;
+    }
+    
+    public void setPerson(final Person person) {
+        this.person = person;
+    }
+    
+    public Set<Company> getCompanies() {
+        return this.companies;
+    }
+    
+    public void setCompanies(final Set<Company> companies) {
+        this.companies = companies;
+    }
+    
+    @JsonIgnore
+    public String getPlainPwd() {
+        return this.plainPwd;
+    }
+    
+    public void setPlainPwd(final String plainPwd) {
+        this.plainPwd = plainPwd;
+        this.password = this.encryptPassword(plainPwd);
+    }
+    
+    @PostLoad
+    public void onPostLoad() {
+        if (this.password != null) {
+            this.plainPwd = this.decryptPassword(this.password);
+        }
+    }
+    
+    private String encryptPassword(final String text) {
+        try {
+            final SecretKeySpec key = new SecretKeySpec("0123456789abcdef".getBytes("UTF-8"), "AES");
+            final Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            c.init(1, key);
+            return DatatypeConverter.printBase64Binary(c.doFinal(text.getBytes("UTF-8")));
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private String decryptPassword(final String text) {
+        try {
+            final SecretKeySpec key = new SecretKeySpec("0123456789abcdef".getBytes("UTF-8"), "AES");
+            final Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            c.init(2, key);
+            return new String(c.doFinal(DatatypeConverter.parseBase64Binary(text)), "UTF-8");
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = 31 * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+    
+    @Override
+    public String toString() {
+        return "Account [id=" + this.id + ", userName=" + this.userName + ", person=" + this.person + "]";
+    }
+    
+    public Long getActiveCompany() {
+        return this.activeCompany;
+    }
+    
+    public void setActiveCompany(final Long activeCompany) {
+        this.activeCompany = activeCompany;
+    }
 }
+
